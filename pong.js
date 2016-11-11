0d05e87ed80a958ac24803c5aa666a23f7f873aa
Desi Desario <desireedesario@gmail.com>
2016-11-10 18:04:05 -0800

    ball stays on screen

--- pong.js
+++ pong.js
@@ -5,10 +5,23 @@ class Vec {
   }
 }
 
+//rectangle
 class Rect {
   constructor(w, h) {
     this.pos = new Vec;
     this.size = new Vec(w, h);
+  }
+  get left(){
+    return this.pos.x - this.size.x / 2;
+  }
+  get right(){
+    return this.pos.x - this.size.x / 2;
+  }
+  get top(){
+    return this.pos.y - this.size.y / 2;
+  }
+  get bottom(){
+    return this.pos.y - this.size.y / 2;
   }
 }
 
@@ -23,10 +36,39 @@ const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 
 const ball = new Ball;
-console.log(ball);
+// console.log(ball);
+ball.pos.x = 100;
+ball.pos.y = 50;
 
-context.fillStyle = "#000000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+ball.vel.x = 100;
+ball.vel.y = 100;
 
-context.fillStyle = "#ffffff";
-context.fillRect(0, 0, 10, 10);
+let lastTime;
+function callback(millis){
+  if (lastTime){
+    update((millis - lastTime) / 1000);
+  }
+  lastTime = millis;
+  requestAnimationFrame(callback);
+}
+
+function update(dt){
+  ball.pos.x += ball.vel.x * dt;
+  ball.pos.y += ball.vel.y * dt;
+
+  // keep ball on screen
+  if (ball.pos.x < 0 || ball.pos.x > canvas.width){
+    ball.vel.x = -ball.vel.x;
+  }
+  if (ball.pos.y < 0 || ball.pos.y > canvas.height){
+    ball.vel.y = -ball.vel.y;
+  }
+
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+callback();
